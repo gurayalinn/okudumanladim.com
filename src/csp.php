982cc9cdc3e660f968604ebccdab8503e59dc5cf
gurayalinn <gurayalinn@gmail.com>
2023-12-22 22:48:14 +0300

    initial commit

--- src/csp.php
+++
@@ -1,7 +0,0 @@
-    <!-- SECURITY CONFIGS -->
-    <meta name="referrer" content="no-referrer-when-downgrade">
-    <meta http-equiv="x-dns-prefetch-control" content="on" />
-    <meta http-equiv="Cache-Control" content="max-age=31536000" />
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src 'self'; script-src 'self'; child-src 'none';">
-    <meta http-equiv="Strict-Transport-Security" content="max-age=31536000; includeSubDomains; preload">
-    <meta http-equiv="X-Content-Type-Options" content="nosniff">
