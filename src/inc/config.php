9b7b2fd6ab6dec0e8d37a47579b30deaece23eb4
gurayalinn <gurayalinn@gmail.com>
2023-12-22 13:29:15 +0300

    initial commit

---
+++ src/inc/config.php
@@ -0,0 +1,10 @@
+<?php
+// Database configuration
+
+$host = $_ENV["MYSQL_HOST"];
+$user = $_ENV["MYSQL_USER"];
+$pass = $_ENV["MYSQL_PASSWORD"];
+$db = $_ENV["MYSQL_DATABASE"];
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
+
