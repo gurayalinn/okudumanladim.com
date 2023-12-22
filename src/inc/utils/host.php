4c325de3d32e2ba05459fe6a98bb9a49753d799e
gurayalinn <gurayalinn@gmail.com>
2023-12-22 13:45:17 +0300

    initial commit

--- src/inc/utils/host.php
+++
@@ -1,6 +0,0 @@
-
-<small><code>
-<?php echo 'REMOTE: ' .$_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT']; ?>
-<br/>
-<?php echo 'SERVER: ' .$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT']; ?>
-</code></small>
