5e707f6e1eee75f747ed246e3a38c9f4e30f3df1
gurayalinn <gurayalinn@gmail.com>
2023-12-22 22:44:28 +0300

    initial commit

---
+++ src/analytics.php
@@ -0,0 +1,41 @@
+
+
+ <!-- Google Analytics
+    <meta name="google-analytics" content="G-" />
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-"
+    ></script>
+    <script>
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+      gtag('config', 'GT-')
+    </script>
+    <script>
+      window.ga =
+        window.ga ||
+        function () {
+          ;(ga.q = ga.q || []).push(arguments)
+        }
+      ga.l = +new Date()
+      ga('create', 'GTM-', 'auto')
+      ga('send', 'pageview')
+    </script>
+    <script>
+      window.ga = function () {
+        ga.q.push(arguments)
+      }
+      ga.q = []
+      ga.l = +new Date()
+      ga('create', 'UA-XXXXX-Y', 'auto')
+      ga('set', 'anonymizeIp', true)
+      ga('set', 'transport', 'beacon')
+      ga('send', 'pageview')
+    </script>
+    <script async src="https://www.google-analytics.com/analytics.js"></script>
+ End Google Analytics -->
+
+<script async src="https://eu.umami.is/script.js" data-website-id="e6b24ed7-3de7-4939-adbf-b82a8e1dfe2d"></script>
---
+++ src/og.php
@@ -0,0 +1,49 @@
+    <!-- OPENGRAPH -->
+    <meta property="og:title" content="okudumanladim.com" />
+    <meta property="og:description" content="Bilgi güvenliği farkındalığı artırmaya yönelik interaktif web uygulaması" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://okudumanladim.com" />
+    <meta property="og:alternate" content="https://okudumanladim.com/" />
+    <meta property="og:headline" content="okudumanladim.com" />
+    <meta property="og:author" content="Güray ALIN" />
+    <meta property="og:published_time" content="2024-01-10T08:20:00+03:00" />
+    <meta property="og:modified_time" content="2024-01-10T09:20:00+03:00" />
+    <meta property="og:updated_time" content="2024-01-10T10:20:00+03:00" />
+
+    <meta
+      property="og:image"
+      content="https://okudumanladim.com/assets/images/favicon.svg"
+    />
+    <meta property="og:image:alt" content="okudumanladim.com" />
+    <meta property="og:image:width" content="512" />
+    <meta property="og:image:height" content="512" />
+    <meta property="og:site_name" content="okudumanladim.com" />
+    <meta property="og:locale" content="tr_TR" />
+    <meta property="og:locale:alternate" content="en_US" />
+    <!-- JSON-LD -->
+    <script type="application/ld+json">
+      {
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": "okudumanladim.com",
+        "alternateName": "okudumanladim.com",
+        "url": "https://okudumanladim.com/",
+        "description": "Bilgi güvenliği farkındalığı artırmaya yönelik interaktif web uygulaması.",
+        "headline": "okudumanladim.com",
+        "mainEntityOfPage": "https://okudumanladim.com/",
+        "image": "https://okudumanladim.com/assets/images/favicon.svg",
+        "datePublished": "2024-01-10T08:20:00+03:00",
+        "dateModified": "2024-01-10T09:20:00+03:00"
+      }
+    </script>
+
+        <!-- TWITTER -->
+    <meta name="twitter:title" content="okudumanladim.com" />
+    <meta name="twitter:description" content="Bilgi güvenliği farkındalığı artırmaya yönelik interaktif web uygulaması" />
+    <meta name="twitter:url" content="https://okudumanladim.com/" />
+    <meta name="twitter:image" content="assets/images/favicon.svg" />
+    <meta name="twitter:image:alt" content="okudumanladim.com" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:creator" content="@gurayalin" />
+    <meta name="twitter:site" content="@gurayalin" />
+    <meta name="twitter:account_id" content="1493274529774321666" />
