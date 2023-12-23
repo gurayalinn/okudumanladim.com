846531e9b6838fd7fe4fd2872f55b18e87b68dca
gurayalinn <gurayalinn@gmail.com>
2023-12-23 13:11:04 +0300

    initial commit

--- src/inc/layout/head/og.php
+++ src/inc/layout/head/og.php
@@ -11,7 +11,7 @@
     <meta property="og:modified_time" content="2024-01-10T09:20:00+03:00" />
     <meta property="og:updated_time" content="2024-01-10T10:20:00+03:00" />
 
-    <meta property="og:image" content="https://okudumanladim.com/assets/images/favicon.svg" />
+    <meta property="og:image" content="https://okudumanladim.com/public/assets/images/favicon.svg" />
     <meta property="og:image:alt" content="okudumanladim.com" />
     <meta property="og:image:width" content="512" />
     <meta property="og:image:height" content="512" />
@@ -29,7 +29,7 @@
   "description": "Bilgi güvenliği farkındalığı artırmaya yönelik interaktif web uygulaması.",
   "headline": "okudumanladim.com",
   "mainEntityOfPage": "https://okudumanladim.com/",
-  "image": "https://okudumanladim.com/assets/images/favicon.svg",
+  "image": "https://okudumanladim.com/public/assets/images/favicon.svg",
   "datePublished": "2024-01-10T08:20:00+03:00",
   "dateModified": "2024-01-10T09:20:00+03:00"
 }
@@ -40,7 +40,7 @@
     <meta name="twitter:description"
       content="Bilgi güvenliği farkındalığı artırmaya yönelik interaktif web uygulaması" />
     <meta name="twitter:url" content="https://okudumanladim.com/" />
-    <meta name="twitter:image" content="assets/images/favicon.svg" />
+    <meta name="twitter:image" content="public/assets/images/favicon.svg" />
     <meta name="twitter:image:alt" content="okudumanladim.com" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:creator" content="@gurayalin" />
